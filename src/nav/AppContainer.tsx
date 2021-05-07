21b898e1eeabbf2fa25b60d922f8534fdac856aa
StudyTae <dea972@naver.com>
2021-05-07 18:14:01 +0900

    앱컨테이너 스크린 옵션 추가

--- src/nav/AppContainer.tsx
+++ src/nav/AppContainer.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator, StackNavigationOptions } from '@react-navigation/stack';
+import { BottomTabBarOptions, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 //screens
 import SearchIndex from '@pages/tabs/search';
@@ -12,6 +12,14 @@ import WebViewIndex from '@pages/webview';
 const CoverModal = createStackNavigator();
 
 const Tab = createBottomTabNavigator();
+
+const tabBarOptions : BottomTabBarOptions = {
+  activeTintColor: 'blue',
+  inactiveTintColor: 'gray',
+  keyboardHidesTabBar: true,
+}
+
+const modalOptions : StackNavigationOptions = { headerShown: false }
 
 const TabNav = () => (
   <Tab.Navigator
@@ -31,11 +39,7 @@ const TabNav = () => (
         return <Image source={iconName} style={{ width: 25, height: 25 }} />;
       },
     })}
-    tabBarOptions={{
-      activeTintColor: 'blue',
-      inactiveTintColor: 'gray',
-      keyboardHidesTabBar: true,
-    }}
+    tabBarOptions={tabBarOptions}
   >
     <Tab.Screen name="검색" component={SearchIndex} />
     <Tab.Screen name="클립한 뉴스" component={ClipsIndex} />
@@ -45,7 +49,7 @@ const TabNav = () => (
 const AppContainer = () => {
   return (
     <NavigationContainer>
-      <CoverModal.Navigator mode="modal" screenOptions={{ headerShown: false }}>
+      <CoverModal.Navigator mode="modal" screenOptions={modalOptions}>
         <CoverModal.Screen name="Tabs" component={TabNav} />
         <CoverModal.Screen name="Webview" component={WebViewIndex} />
       </CoverModal.Navigator>
